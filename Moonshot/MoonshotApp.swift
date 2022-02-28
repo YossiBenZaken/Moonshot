1e7659fec0c06be0e42aa80bd4350fb2fb048155
YossiBenZaken <yossibz@gmail.com>
2022-02-28 20:25:05 +0200

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Yosef Ben Zaken on 28/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
